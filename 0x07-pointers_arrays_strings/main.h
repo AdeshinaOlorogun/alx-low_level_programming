b76ef17e7566ce34eea611b048426ce46b28558f
Adeshina Olorogun <adeshinamolorogun@gmail.com>
2023-10-23 07:39:37 -0700

    Aunty betty is that you?

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,9 +3,9 @@
 
 
 #include <stdio.h>
-#include  <stdlib.h>
+#include <stdlib.h>
 int _putchar(char c);
-char *_memset(char *s, char b, unsingned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -15,4 +15,3 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 #endif/*MAIN_H*/
-
