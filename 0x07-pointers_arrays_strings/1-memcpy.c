859299e5db3ac4c001ad383c79996fce41085289
Adeshina Olorogun <adeshinamolorogun@gmail.com>
2023-10-23 07:57:38 -0700

    Squashing bugs

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memcpy - function that copies @n in bytes from memory area @src
+ * _memcpy - function copies @n bytes from memory area @src
  * to memory area @dest
  *
  * @n: function copies
@@ -10,7 +10,7 @@
  *
  * @dest: to memory area
  *
- * return: a pointer to @dest
+ * Return: a pointer to @dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
