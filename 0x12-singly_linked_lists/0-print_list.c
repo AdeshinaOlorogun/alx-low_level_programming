8a5e200f024bf3a3ae22fb2020f36b77c1cba10c
Adeshina Olorogun <adeshinamolorogun@gmail.com>
2023-11-16 06:44:47 -0800

    Refix 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,25 +5,25 @@
 
 /**
  * print_list - check the code for Holberton School students.
- * @i: name of the list
+ * @h: name of the list
  * Return: the number of nodes.
  */
-size_t print_list(const list_t *i)
+size_t print_list(const list_t *h)
 {
 	int count = 0;
 
-	while (i)
+	while (h)
 	{
-		if (i->str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-		printf("[%d] %s\n", i->len, i->str);
+		printf("[%d] %s\n", h->len, h->str);
 		}
 		count++;
-		i = i->next;
+		h = h->next;
 	}
 	return (count);
 }
