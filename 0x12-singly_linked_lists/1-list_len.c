6ce175925dad8425217e8b30a2266b3e11433d22
Adeshina Olorogun <adeshinamolorogun@gmail.com>
2023-11-16 06:50:58 -0800

    2-add_node.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,17 +5,17 @@
 
 /**
  * list_len - check the code for Holberton School students.
- * @i: name of the list
+ * @h: name of the list
  * Return: the number of nodes.
  */
-size_t list_len(const list_t *i)
+size_t list_len(const list_t *h)
 {
 	int count = 0;
 
-	while (i)
+	while (h)
 	{
 		count++;
-		i = i->next;
+		h = h->next;
 	}
 	return (count);
 }
