d35b8374a20f4b42154abd99fd630423adde755f
Adeshina Olorogun <adeshinamolorogun@gmail.com>
2023-11-23 16:05:23 -0800

    Added 5-flip_bits.c file

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - Count the number of bits needed to flip from one number to another.
+ * flip_bits - Counts the number of bits needed to flip from one number to another.
  * @n: First number
  * @m: Second number
  * Return: Number of bits needed to flip
@@ -10,11 +10,13 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int difference = n ^ m;
 	unsigned int count = 0;
+	unsigned int size = sizeof(unsigned long int) * 8;
 
-	while (difference > 0)
+	while (size > 0)
 	{
 		count += difference & 1;
 		difference >>= 1;
+		size--;
 	}
 
 	return count;
