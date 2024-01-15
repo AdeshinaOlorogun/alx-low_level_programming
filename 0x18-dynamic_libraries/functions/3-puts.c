5083cab481ac65abab9e40f3f94eb6dbbbbe9cc1
Adeshina Olorogun <adeshinamolorogun@gmail.com>
2024-01-15 09:48:02 -0800

    Created libdynamic library

---
+++ 0x18-dynamic_libraries/functions/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * _puts - Prints a string followed by a new line to stdout
+ * @str: The string to be printed
+ */
+void _puts(char *str);
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
